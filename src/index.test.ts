6c8ec61bfc01dd5a1f1936901cd9f577e19695e0
NabilBoubekri <nabil.boubekri.sio@gmail.com>
2026-01-29 14:41:26 +0100

    ajout de test pour la figure Carré

--- src/index.test.ts
+++ src/index.test.ts
@@ -3,8 +3,14 @@ import { getYamsPoints } from './index';
 
 describe("get points for a dice roll of Yams ", () => {
     it("should return Brelan (28 points) for a roll of [4, 4, 4, 2, 5]", () => {
-        const roll = [[4, 4, 4, 2, 5]];
-        const points = getYamsPoints(roll);
+        const rolls = [[4, 4, 4, 2, 5]];
+        const points = getYamsPoints(rolls);
         expect(points).toBe(28);
+    });
+
+    it("should return Carré (35 points) for a roll of [6, 6, 6, 6, 2]", () => {
+        const rolls = [[6, 6, 6, 6, 2]];
+        const points = getYamsPoints(rolls);
+        expect(points).toBe(35);
     });
 });
